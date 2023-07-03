a5d6772ddbd96796ae7e63fa1edaf2d58dd3ceb3
Besufekad-HAZ <besurye@gmail.com>
2023-07-03 18:27:56 +0300

    Fix some autocorrectable linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Food < ApplicationRecord
-  belongs_to :user, class_name: "User", foreign_key: "user_id"
-  has_many :recipe_foods, class_name: "RecipeFood", foreign_key: "food_id"
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id'
   has_many :recipes, through: :recipe_foods
 
   validates :name, presence: true
