bfa2d25adc122a6e6ec7cffbef9ee8abc9a9a4ca
Besufekad-HAZ <besurye@gmail.com>
2023-07-04 13:15:56 +0300

    Fix merge conflicts,migrations and rubocop linter errors

--- db/migrate/20230704095138_create_recipe_foods.rb
+++ db/migrate/20230704095138_create_recipe_foods.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
@@ -7,5 +9,9 @@ class CreateRecipeFoods < ActiveRecord::Migration[7.0]
 
       t.timestamps null: false, default: -> { 'CURRENT_TIMESTAMP' }
     end
+    add_foreign_key :recipe_foods, :recipes, column: :recipe_id
+    add_foreign_key :recipe_foods, :foods, column: :food_id
+    add_index :recipe_foods, :recipe_id
+    add_index :recipe_foods, :food_id
   end
 end
