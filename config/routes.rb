51a4796e91330ec6e91b53c5f01e36bb6f13870b
Hyder <hyderali400@gmail.com>
2023-07-04 15:26:17 +0530

    add models , controllers and views as per ER diagram

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :foods
+  resources :recipe_foods
+  resources :recipes
   devise_for :users
+  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
