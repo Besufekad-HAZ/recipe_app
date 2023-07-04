e03dbfd3cc8f51198cca4f97679ba851f6772493
Besufekad-HAZ <besurye@gmail.com>
2023-07-04 17:38:57 +0300

    Update food controllers,routes and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include Devise::Controllers::Helpers
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  protected
+
+  helper_method :sort_column, :sort_direction
+  include ApplicationHelper
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- test/controllers/foods_controller_test.rb
+++ test/controllers/foods_controller_test.rb
@@ -1,52 +1,33 @@
-require 'test_helper'
+require "test_helper"
 
 class FoodsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @food = foods(:one)
-  end
-
-  test 'should get index' do
-    get foods_url
+  test "should get index" do
+    get foods_index_url
     assert_response :success
   end
 
-  test 'should get new' do
-    get new_food_url
+  test "should get show" do
+    get foods_show_url
     assert_response :success
   end
 
-  test 'should create food' do
-    assert_difference('Food.count') do
-      post foods_url,
-           params: { food: { measurement_unit: @food.measurement_unit, name: @food.name, price: @food.price,
-                             quantity: @food.quantity, user_id: @food.user_id } }
-    end
-
-    assert_redirected_to food_url(Food.last)
-  end
-
-  test 'should show food' do
-    get food_url(@food)
+  test "should get new" do
+    get foods_new_url
     assert_response :success
   end
 
-  test 'should get edit' do
-    get edit_food_url(@food)
+  test "should get create" do
+    get foods_create_url
     assert_response :success
   end
 
-  test 'should update food' do
-    patch food_url(@food),
-          params: { food: { measurement_unit: @food.measurement_unit, name: @food.name, price: @food.price,
-                            quantity: @food.quantity, user_id: @food.user_id } }
-    assert_redirected_to food_url(@food)
+  test "should get edit" do
+    get foods_edit_url
+    assert_response :success
   end
 
-  test 'should destroy food' do
-    assert_difference('Food.count', -1) do
-      delete food_url(@food)
-    end
-
-    assert_redirected_to foods_url
+  test "should get destroy" do
+    get foods_destroy_url
+    assert_response :success
   end
 end
