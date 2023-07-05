63139e129805e346d41b416686ac0317099c378e
Besufekad-HAZ <besurye@gmail.com>
2023-07-05 14:50:00 +0300

    Update recipes controller create method adding @recipe.user = current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # layout 'standard'
   include Devise::Controllers::Helpers
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
