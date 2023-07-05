dcb61c1d66b8639b80f4acf9b323697028e1299c
Besufekad-HAZ <besurye@gmail.com>
2023-07-05 17:16:04 +0300

    Remove unneccesary commented lines from App controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  # layout 'standard'
   include Devise::Controllers::Helpers
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
