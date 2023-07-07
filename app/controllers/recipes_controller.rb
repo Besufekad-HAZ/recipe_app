5dabf5560683453d37b5029e8dde0ae0a7ab9c3b
Hyder <hyderali400@gmail.com>
2023-07-07 13:28:36 +0530

    update recipe datials and layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,10 @@ class RecipesController < ApplicationController
     if @recipe.save
       food_quantities = recipe_params[:food_quantities]
       food_quantities&.each do |food_id, quantity|
-        @recipe.recipe_foods.create(food_id: food_id, quantity: quantity.to_i, recipe_id: @recipe.id) unless quantity.to_i < 1
+        unless quantity.to_i < 1
+          @recipe.recipe_foods.create(food_id:, quantity: quantity.to_i,
+                                      recipe_id: @recipe.id)
+        end
       end
       redirect_to recipes_path, notice: 'Recipe was successfully created.'
     else
@@ -40,7 +43,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     if current_user == @recipe.user
       @recipe.update(public: !@recipe.public?)
-      flash.notice = "Recipe privacy has been updated."
+      flash.notice = 'Recipe privacy has been updated.'
 
       # Add the following line to set the public attribute to true
       @recipe.update(public: true) if @recipe.public?
@@ -51,9 +54,9 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id, food_quantities: {})
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id,
+                                   food_quantities: {})
   end
-
 
   def create_recipe_foods(recipe, food_quantities)
     return unless food_quantities.is_a?(Hash)
@@ -61,7 +64,7 @@ class RecipesController < ApplicationController
     food_quantities.each do |food_id, quantity|
       next if quantity.to_i < 1
 
-      recipe.recipe_foods.create(food_id: food_id, quantity: quantity.to_i)
+      recipe.recipe_foods.create(food_id:, quantity: quantity.to_i)
     end
   end
 end
