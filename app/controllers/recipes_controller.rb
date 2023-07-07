9ce5bc1729318ea7420abddc7d59b017f8f24a2a
Besufekad-HAZ <besurye@gmail.com>
2023-07-07 14:12:38 +0300

    Fix Rubocop linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,10 @@ class RecipesController < ApplicationController
     if @recipe.save
       food_quantities = recipe_params[:food_quantities]
       food_quantities&.each do |food_id, quantity|
-        @recipe.recipe_foods.create(food_id: food_id, quantity: quantity.to_i, recipe_id: @recipe.id) unless quantity.to_i < 1
+        unless quantity.to_i < 1
+          @recipe.recipe_foods.create(food_id:, quantity: quantity.to_i,
+                                      recipe_id: @recipe.id)
+        end
       end
       redirect_to recipes_path, notice: 'Recipe was successfully created.'
     else
@@ -45,9 +48,9 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id, food_quantities: {})
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id,
+                                   food_quantities: {})
   end
-
 
   def create_recipe_foods(recipe, food_quantities)
     return unless food_quantities.is_a?(Hash)
@@ -55,7 +58,7 @@ class RecipesController < ApplicationController
     food_quantities.each do |food_id, quantity|
       next if quantity.to_i < 1
 
-      recipe.recipe_foods.create(food_id: food_id, quantity: quantity.to_i)
+      recipe.recipe_foods.create(food_id:, quantity: quantity.to_i)
     end
   end
 end
